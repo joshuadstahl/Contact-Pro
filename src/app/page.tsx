d443b3283ba98371666a4c75e480396fab34c4ea
azadmin <47959058+95josh@users.noreply.github.com>
2025-01-13 09:08:18 -0700

    temporary link on home page to go to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="top-0 bottom-0 left-0 right-0 absolute flex p-2.5">
-      
+      <h1>Contact Pro</h1>
+      <a href="/app">Go To App</a>
     </div>
   );
 }
